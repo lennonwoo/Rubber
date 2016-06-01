3970a1553179abc700738944368b22ca659b611b
lennonwoo <cccmorin@gmail.com>
2016-06-01 14:34:28 +0800

    initialize project

---
+++ app/src/main/java/com/lennonwoo/rubber/BaseView.java
@@ -0,0 +1,10 @@
+package com.lennonwoo.rubber;
+
+/**
+ * Created by lennon on 6/1/16.
+ */
+public interface BaseView<T> {
+
+    void setPresenter(T presenter);
+
+}
---
+++ app/src/main/java/com/lennonwoo/rubber/MainActivity.java
@@ -0,0 +1,53 @@
+package com.lennonwoo.rubber;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+/**
+ * Created by lennon on 6/1/16.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        DrawerLayout drawerLayout = (DrawerLayout)  findViewById(R.id.drawer_layout);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu);
+        }
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, 0, 0) {
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                invalidateOptionsMenu();
+                syncState();
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu();
+                syncState();
+            }
+
+        };
+        if (drawerLayout != null) {
+            drawerLayout.addDrawerListener(drawerToggle);
+        }
+    }
+
+}
