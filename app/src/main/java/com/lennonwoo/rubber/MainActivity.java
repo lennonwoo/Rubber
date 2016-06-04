b9c369d144f79ad343705ea9d3fc81df5d160474
lennonwoo <cccmorin@gmail.com>
2016-06-04 16:09:38 +0800

    add butterknife to replace findViewById and setOnClickListener

--- app/src/main/java/com/lennonwoo/rubber/MainActivity.java
+++ app/src/main/java/com/lennonwoo/rubber/MainActivity.java
@@ -6,23 +6,32 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by lennon on 6/1/16.
  */
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         init();
     }
 
     private void init() {
-        DrawerLayout drawerLayout = (DrawerLayout)  findViewById(R.id.drawer_layout);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -48,6 +57,16 @@ public class MainActivity extends AppCompatActivity {
         if (drawerLayout != null) {
             drawerLayout.addDrawerListener(drawerToggle);
         }
+
+        getFragmentManager()
+                .beginTransaction()
+                .replace(R.id.main_content_fragment, new SongsListFragment())
+                .commit();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.tasks_fragment_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 }
